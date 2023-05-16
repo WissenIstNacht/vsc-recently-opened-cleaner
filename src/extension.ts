a29886496af2ef4bfa8908825aaece855a5a888d
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-05-16 09:24:37 +0200

    Feature: Clean recently opened command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "recently-opened-cleaner" is now active!'
-  );
+type URI = vscode.Uri;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+// The following interfaces are copied from
+// https://github.com/microsoft/vscode/blob/9ff83d41a3268ec578c032ae54102285b447e947/src/vs/platform/workspaces/common/workspaces.ts#L48
+export interface IRecentlyOpened {
+  workspaces: IRecentFolder[];
+  files: IRecentFile[];
+}
+
+export type IRecent = IRecentFolder | IRecentFile;
+
+interface IRecentFolder {
+  readonly folderUri: URI;
+  label?: string;
+  readonly remoteAuthority?: string;
+}
+
+interface IRecentFile {
+  readonly fileUri: URI;
+  label?: string;
+  readonly remoteAuthority?: string;
+}
+
+export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-    'recently-opened-cleaner.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello World from recently-opened-cleaner!'
+    'recently-opened-cleaner.clean-recently-opened',
+    async () => {
+      let recentEntries = (await vscode.commands.executeCommand(
+        '_workbench.getRecentlyOpened'
+      )) as IRecentlyOpened;
+
+      const filtered = recentEntries.workspaces.filter(
+        (ws) => !isRecentlyOpenedValid(ws.folderUri)
       );
+      const initialEntriesCount = recentEntries.workspaces.length;
+      const toDeleteCount = filtered.length;
+
+      filtered.forEach((ws) => {
+        console.log('Deleting ' + ws.folderUri.toString());
+        try {
+          vscode.commands.executeCommand(
+            'vscode.removeFromRecentlyOpened',
+            ws.folderUri.fsPath
+          );
+        } catch (e) {
+          console.error(
+            'something went wrong for the folder with URI: ' +
+              ws.folderUri +
+              ' The error returned: ' +
+              e
+          );
+        } finally {
+          console.log('Deletion done');
+        }
+      });
+
+      recentEntries = (await vscode.commands.executeCommand(
+        '_workbench.getRecentlyOpened'
+      )) as IRecentlyOpened;
+      const newEntriesCount = recentEntries.workspaces.length;
+
+      if (initialEntriesCount - toDeleteCount === newEntriesCount) {
+        console.log('success');
+      } else {
+        console.error(
+          'From the initial ' +
+            initialEntriesCount +
+            ' entries, ' +
+            toDeleteCount +
+            ' entries should have been deleted. Effectively deleted: ' +
+            newEntriesCount
+        );
+      }
     }
   );
 
@@ -30,3 +82,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function isRecentlyOpenedValid(uri: URI) {
+  return uri.scheme === 'vscode-vfs' || fs.existsSync(uri.fsPath);
+}
